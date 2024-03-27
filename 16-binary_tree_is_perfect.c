7e70d7853c1f1c0f7378df572cb8253161785cb8
helmogey <h.elmogey@gmail.com>
2024-03-27 13:15:38 +0200

    1st

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,29 +1,6 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
-
-
-/**
- * is_perfect_util - function
- * @node: struct
- * @level: int
- * Return: new_node
- */
-
-int is_perfect_util(const binary_tree_t *node, int level)
-{
-if (node == NULL)
-{
-return (level == 0);
-}
-
-if ((node->left == NULL) != (node->right == NULL))
-{
-return (0);
-}
-
-return (is_perfect_util(node->left, level + 1) &&
-is_perfect_util(node->right, level + 1));
-}
 
 /**
  * binary_tree_is_perfect - function
@@ -38,6 +15,41 @@ if (tree == NULL)
 {
 return (0);
 }
-
+int is_perfect_util(const binary_tree_t *node, int level);
 return (is_perfect_util(tree, 0));
+}
+
+
+
+/**
+ * is_perfect_util - function
+ * @node: struct
+ * @level: int
+ * Return: new_node
+ */
+
+int is_perfect_util(const binary_tree_t *node, int level)
+{
+    
+if (node == NULL)
+{
+return (1);
+}
+if ((node->left == NULL) != (node->right == NULL))
+{
+return (0);
+}
+
+// if ((node->left->parent != node) || (node->right->parent != node))
+// {
+// return (0);
+// }
+
+if ((node->left == NULL) != (node->right == NULL))
+{
+return (0);
+}
+
+return (is_perfect_util(node->left, level + 1) &&
+is_perfect_util(node->right, level + 1));
 }
