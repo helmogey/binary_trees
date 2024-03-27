bfcbee349557d8b33a135108129f19b2fab3b390
helmogey <h.elmogey@gmail.com>
2024-03-27 13:17:51 +0200

    1st

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -3,25 +3,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_perfect - function
- * @tree: struct
- * Return: new_node
- */
-
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-/* Check for empty tree */
-if (tree == NULL)
-{
-return (0);
-}
-int is_perfect_util(const binary_tree_t *node, int level);
-return (is_perfect_util(tree, 0));
-}
-
-
-
-/**
  * is_perfect_util - function
  * @node: struct
  * @level: int
@@ -48,4 +29,20 @@ return (0);
 
 return (is_perfect_util(node->left, level + 1) &&
 is_perfect_util(node->right, level + 1));
+}
+
+/**
+ * binary_tree_is_perfect - function
+ * @tree: struct
+ * Return: new_node
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+/* Check for empty tree */
+if (tree == NULL)
+{
+return (0);
+}
+return (is_perfect_util(tree, 0));
 }
