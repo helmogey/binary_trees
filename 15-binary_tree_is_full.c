5bb3340f94801c94c3ebc5c01e477c32b3bfc6e7
helmogey <h.elmogey@gmail.com>
2024-03-27 13:45:45 +0200

    1st

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+
+
+/**
+ * is_full_util - function
+ * @node: struct
+ * Return: new_node
+ */
+
+int is_full_util(const binary_tree_t *node)
+{
+if (node == NULL)
+{
+return (1);
+}
+
+
+if ((node->left != NULL && node->right == NULL) ||
+(node->left == NULL && node->right != NULL) ||
+is_full_util(node->left) == 0 ||
+is_full_util(node->right) == 0)
+{
+return (0);
+}
+return(1);
+}
+
+
+/**
+ * binary_tree_is_full - function
+ * @tree: struct
+ * Return: new_node
+ */
+
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+if (tree == NULL)
+{
+return (0);
+}
+
+return (is_full_util(tree));
+}
+
--- binary_trees.h
+++ binary_trees.h
@@ -25,5 +25,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 #endif /* _BINARY_TREES_H_ */
