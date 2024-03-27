f84ba627f501356564eeed250a400b57d8c5465c
helmogey <h.elmogey@gmail.com>
2024-03-27 20:34:00 +0200

    1st

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+
+
+/**
+ * binary_tree_postorder - function
+ * @tree: struct
+ * @func: int
+ * Return: new_node
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (tree == NULL || func == NULL)
+{
+return;
+}
+
+binary_tree_postorder(tree->left, func);
+
+binary_tree_postorder(tree->right, func);
+
+func(tree->n);
+}
