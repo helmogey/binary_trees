39e57972b18720766cdd59ae0ec95a5cecb7f771
helmogey <h.elmogey@gmail.com>
2024-03-27 15:00:53 +0200

    1st

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -5,36 +5,19 @@
 
 /**
  * binary_tree_depth - function
- * @root: struct
- * @node: struct
+ * @tree: struct
  * Return: new_node
  */
-size_t binary_tree_depth(const binary_tree_t *root, const binary_tree_t *node)
+size_t binary_tree_depth(const binary_tree_t *tree)
 {
 size_t left_depth, right_depth;
-
-if (root == NULL)
+if (tree == NULL)
 {
 return (0);
 }
 
-if (root == node)
-{
-return (0);
-}
+left_depth = binary_tree_depth(tree->left);
+right_depth = binary_tree_depth(tree->right);
 
-left_depth = binary_tree_depth(root->left, node);
-right_depth = binary_tree_depth(root->right, node);
-
-if (left_depth > 0)
-{
-return (1 + left_depth);
-}
-
-if (right_depth > 0)
-{
-return (1 + right_depth);
-}
-
-return (0);
+return (1 + (left_depth > right_depth ? left_depth : right_depth));
 }
